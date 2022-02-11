9a3875bf9a7cab4a6b893054c2e58de9c774dc89
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2022-02-11 14:11:14 +0100

    updated js

---
+++ js/question-1.js
@@ -0,0 +1,6 @@
+//Question 1 
+// Convert the function below to an arrow function: 
+// function getRemainder(a, b) { return a % b;}
+const getRemainder = (a, b) => a % b;
+const result = getRemainder(2, 6);
+console.log(result);
